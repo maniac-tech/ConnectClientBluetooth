71fa8d8247271aba18c2b9984f619d2f58b1080f
maniac-tech <ajj_2396@yahoo.com>
2017-04-25 21:18:37 +0530

    first commit

---
+++ app/src/main/java/com/example/maniac/connectclientbluetooth/ConnectedThread.java
@@ -0,0 +1,31 @@
+package com.example.maniac.connectclientbluetooth;
+
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Created by maniac on 4/25/2017.
+ */
+
+//public class MyBluetoothService {
+
+
+    // Defines several constants used when transmitting messages between the
+    // service and the UI.
+//    private interface MessageConstants {
+//        public static final int MESSAGE_READ = 0;
+//        public static final int MESSAGE_WRITE = 1;
+//        public static final int MESSAGE_TOAST = 2;
+//
+//        // ... (Add other message types here as needed.)
+//    }
+//
+//
+//}
